ac7d1a59b37d4806cd678056528a812ff9e9d7bc
Ren Daikyuu <ren.daikyuu@gmail.com>
2022-12-28 08:08:22 +0700

    init file

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Xin chào công chúa ! 🖐🖐🖐',
+    introDesc: `Minh Châu có vài điều muốn hỏi bạn, bạn nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'Ukie',
+    title: 'Mình bên nhau gần 1 năm rồi, bạn có hạnh phúc khi ở bên mình hem? 😙',
+    desc: 'Bạn trả lời không hoặc thoát ra thì mông sẽ teo lại kakaka:> ',
+    btnYes: 'Có, mình rất rất dui💖💖💖',
+    btnNo: '💔 Không hiề 💔 ',
+    question:'Vậy nói cho mình biết điều ước năm mới của bạn đi 😊😊',
+    reasonPlaceholder: 'Điều ước năm mới của Chúc Chi',
+    btnReply: 'Gửi ! 💌💌',
+    reply: 'Ước gì 2023 cũng được ở bên Minh Châu cùng nhiều năm sau nữa 😊😊',
+    mess: 'Mình biết mà, mình cũng vậy 🥰. Love you 696.969 😘😘',
+    messDesc: '💬💬 Nhắn tin cho mình nha nha. 💬💬',
+    btnAccept: 'Okie lun 💖',
+    messLink: 'https://m.me/Ren.Daikyuu'
+}
